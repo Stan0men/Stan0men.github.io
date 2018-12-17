995bc278c209a85926845ef00513106e37db534a
Stan <stan0men@yahoo.com>
2018-12-17 18:04:30 +0200

    add solution for this_practice

--- js_this_practice/script.js
+++ js_this_practice/script.js
@@ -1,46 +1,35 @@
 function Person(name, date, amount) {
     this.name = name;
-    this.birthDate = date;
-    this.money = amount;
+    this.date = date;
+    this.amount = amount;
     this.transactionHistory=[`Start count: ${this.amount}`];
+
+    let getAge = function(){
+        const dateParts = date.split('.');
+        const birthDate = new Date(dateParts[2], dateParts[1]-1, dateParts[0]);
+        const diff= Date.now()-birthDate.getTime();
+        return Math.abs(new Date(diff).getUTCFullYear()-1970);
+    };
 
 
     this.getInfo=function(){
-        return `Person ${this.name} was born at ${this.birthDate} and has on his account ${this.money} dollars` ;
-    }
+        return `Person ${this.name} was born at ${this.birthDate} and has on his account ${this.amount} dollars` ;
+    };
     this.addMoney=function(amount, comment){
         this.amount += amount;
         this.transactionHistory.push(`${comment}: ${amount}`);
-
-    }
+        return `Person ${this.name} has on his account ${this.amount} dollars and add ${amount} with comment: ${comment}`
+    };
     this.withdrawMoney=function(withdrawAmount, comment){
         this.amount -= withdrawAmount;
-        this.transactionHistory.push(`${comment}: -${withdrawAmount}`)
+        this.transactionHistory.push(`${comment}: -${withdrawAmount}`);
+        return `Person ${this.name} has on his account ${this.amount} dollars and minus ${withdrawAmount}  with comment: ${comment}`
 
     }
 }
 
-let papka = new Person("Sashok", "16.02.1990");
-
-
-// let papka = {
-//     age: 28,
-//     name: "Sasha",
-//     birthDate: "16.02.1990",
-//     money: 100500,
-//     getInfo: function () {
-//         console.log(`Person ${this.name} was born at ${this.birthDate} and has on his account ${this.money} dollars`)
-//     },
-//     addMoney: function (addMoney) {
-//         let res= addMoney+this.money;
-//         console.log(`Money on the bank account of person ${this.name} was increased on ${addMoney} and is ${res} dollars`);
-//     },
-//     withdrawMoney: function (minusMoney) {
-//         let res= this.money-minusMoney;
-//         console.log(`Money on the bank account of person ${this.name} was reduced on ${minusMoney} and is ${res} dollars`);
-//     }
-// };
-//
-// papka.getInfo();
-// papka.addMoney(500);
-// papka.withdrawMoney(600);
+let papka = new Person("Sashok", "16.02.1990", 100500);
+console.log(papka.getAge());
+//console.log(papka.getInfo());
+//console.log(papka.addMoney(500, "Allo"));
+//console.log(papka.withdrawMoney(1000, "Ku ku!!!"));
